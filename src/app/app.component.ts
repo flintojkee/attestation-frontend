b3d05c9789c0914cb614d7bd56b20dc534f91210
DenysVasylenko <32224385+flintojkee@users.noreply.github.com>
2019-02-18 00:29:51 +0200

    test api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'attestation-frontend';
+  msg: string;
+  constructor(private appService: AppService) {
+  }
+  ngOnInit() {
+    this.appService.testRoute().subscribe(data =>{
+      this.msg = data['msg']
+      console.log(data);
+    });
+  }
+
 }
